b9e85a3b0711741253047a8ddc5e360f1eab2c36
nagamiya <appngmy@gmail.com>
2020-04-21 13:09:10 +0900

    00, 01 done

---
+++ Python/Chapter1/00_Reversed_String.py
@@ -0,0 +1,4 @@
+# “stressed”を逆にする
+origin_text = "stressed"
+new_text = origin_text[::-1]
+print("original text: %s\nnew text: %s" % (origin_text, new_text) )
---
+++ Python/Chapter1/01_schooled.py
@@ -0,0 +1,3 @@
+# scholledの1,3,5,7文字目を連結
+text = "schooled"
+print("1st, 3rd, 5th, and 7th: %s" % text[0::2])
